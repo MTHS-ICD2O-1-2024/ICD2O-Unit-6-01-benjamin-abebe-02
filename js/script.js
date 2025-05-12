f3948d998e8b707589541e048219dba646f2bf8f
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-12 17:05:01 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// 
+// Created by: Benjamin Abebe
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// process
+function myButtonClicked() {
+  const multiplicandA = parseFloat(document.getElementById("multiplicand").value)
+  const multiplierB = parseFloat(document.getElementById("multiplier").value)
+  let result = 0
+  let count = 0
+
+  /**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICD2O-Unit-5-08-benjamin-abebe-02/sw.js", {
+    scope: "/ICD2O-Unit-5-08-benjamin-abebe-02/",
+  })
+}
+
+  while (count < multiplierB) {
+    result += multiplicandA
+    count++
+  }
+
+  // display results
+  document.getElementById("answer").innerHTML = "<p>The answer is:- " + result + ".</p>"
+}
